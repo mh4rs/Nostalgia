c19e8f7615146dfb174e7fc2016d4f7c4d9a9926
mh4rs <mharissa@umich.edu>
2025-03-20 17:17:55 -0400

    logic and UI adjustments

--- scripts/10-rock-paper-scissors.js
+++ scripts/10-rock-paper-scissors.js
@@ -65,8 +65,8 @@ updateScoreElement();
 document.querySelector('.js-result').innerHTML = result;
 
 document.querySelector('.js-moves').innerHTML = `You
-<img src = images/${playerMove}_emoji.png class="move-icon">
-<img src = images/${computerMove}_emoji.png class="move-icon">
+<img src = images/${playerMove}-emoji.png class="move-icon">
+<img src = images/${computerMove}-emoji.png class="move-icon">
 Computer`;
 
 }
